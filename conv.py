38694b8281c08dc6575ef95d2710451701ef1227
Thomas Roddick <tr346@azara.eng.cam.ac.uk>
2015-11-19 13:13:40 +0000

    Implemented batch normalisation
    Implemented a spatial_batch_normalization method which performs
    batch normalisation as outlined in the Ioffe and Szegedy 2015
    paper.

--- conv.py
+++ conv.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 
-def spatial_conv(inputs, noutput_chls, kW, kH, dW=1, dH=1,
+def spatial_conv(input, noutput_chls, kW, kH, dW=1, dH=1,
     padding = "SAME", init = 1.0, name=None):
     """Convolutional layer which applies a 2D filter over an image.
 
     Args:
-        inputs: 4-D tensor with dimensions [batch_size, height,
-            width, ninput_chls].
+        input: 4-D tensor with dimensions [batch_size, height,
+            width, ninputchls].
         noutput_chls: Number of output channels
         kW: Kernel width
         kH: Kernel height
@@ -22,23 +22,23 @@ def spatial_conv(inputs, noutput_chls, kW, kH, dW=1, dH=1,
         output: 4-D feature map with dimensions [batch_size, h_output,
             w_output, noutput_chls]"""
 
-    ninput_chls = inputs.get_shape().as_list()[3]
+    ninputchls = input.get_shape().as_list()[3]
 
     weights = tf.Variable(
-        tf.truncated_normal([kH, kW, ninput_chls, noutput_chls], stddev = init),
+        tf.truncated_normal([kH, kW, ninputchls, noutput_chls], stddev = init),
         name = 'weights')
     biases = tf.Variable(tf.zeros(noutput_chls), name='biases')
 
-    conv = tf.nn.conv2d(inputs, weights, [1, dH, dW, 1], padding = padding)
+    conv = tf.nn.conv2d(input, weights, [1, dH, dW, 1], padding = padding)
 
 
-def spatial_max_pooling_indices(inputs, kW, kH, dW=None, dH=None,
+def spatial_max_pooling_indices(input, kW, kH, dW=None, dH=None,
     padding='SAME', name=None):
     """Performs max pooling by taking the largest value in every kH x kW region,
     and outputs max values and pooling indices as tensors.
 
     Args:
-        inputs: 4-D tensor with dimensions [batch_size, height, width, chls].
+        input: 4-D tensor with dimensions [batch_size, height, width, chls].
         kW: Width of region to pool over
         kH: Height of region to pool over
         dW: Horizontal stride (defaults to kW i.e. non-overlapping pooling)
@@ -55,18 +55,18 @@ def spatial_max_pooling_indices(inputs, kW, kH, dW=None, dH=None,
 
     if dW is None: dW = kW
     if dH is None: dH = kH
-    return tf.nn.max_pool_with_argmax(inputs, [1, kH, kW, 1], [1, dW, dH, 1],
+    return tf.nn.max_pool_with_argmax(input, [1, kH, kW, 1], [1, dW, dH, 1],
         padding, name)
 
 
-def spatial_unpooling(inputs, indices, kW, kH, dW=None, dH=None, padding='SAME',
+def spatial_unpooling(input, indices, kW, kH, dW=None, dH=None, padding='SAME',
     name=None):
     """Reconstructs the input to a pooling operation using the saved pooling
     indices.
 
     Args:
-        inputs: 4-D tensor with dimensions [batch_size, height, width, chls].
-        indices: 4-D tensor with the same dimensions as inputs, containing the
+        input: 4-D tensor with dimensions [batch_size, height, width, chls].
+        indices: 4-D tensor with the same dimensions as input, containing the
             saved pooling indices
         kW: Width of pooling filter
         kH: Height of pooling filter
@@ -81,16 +81,56 @@ def spatial_unpooling(inputs, indices, kW, kH, dW=None, dH=None, padding='SAME',
     if dH is None: dH = kH
 
     # Compute output dimensions
-    output_shape = (tf.shape(inputs) - [0, 1, 1, 0]) * [1, dH, dW, 1] + [0, kH, kW, 0]
+    output_shape = (tf.shape(input) - [0, 1, 1, 0]) * [1, dH, dW, 1] + [0, kH, kW, 0]
     pnt = tf.Print(output_shape, [output_shape])
 
-    # Flatten inputs and indices into 1D vectors
-    inputs_flat = tf.reshape(inputs, [-1])
+    # Flatten input and indices into 1D vectors
+    input_flat = tf.reshape(input, [-1])
     indices_flat = tf.reshape(indices, [-1])
     output_shape_flat = tf.to_int64(tf.reduce_prod(output_shape, keep_dims = True))
 
-    # Populate output with values from inputs
-    output_flat = tf.sparse_to_dense(indices_flat, output_shape_flat, inputs_flat, 0)
+    # Populate output with values from input
+    output_flat = tf.sparse_to_dense(indices_flat, output_shape_flat, input_flat, 0)
     output = tf.reshape(output_flat, output_shape)
+
+    return output
+
+
+def spatial_batch_normalization(input, eps=1e-5, name=None):
+    """Performs batch normalization as described in "Batch Normalization:
+    Accelerating Deep Network Training by Reducing Internal Covariate Shift",
+    Ioffe, Szegedy, 2015
+
+    Args:
+        input: 4-D tensor with dimensions [batch_size, height, width, chls]
+        eps: A small scalar added to the variance to ensure numerical stability
+            (default 1e-5)
+        name: Variable scope for the module
+
+    Returns:
+        output: A batch normalized tensor with the same dimensions as the input
+
+    """
+
+    # Compute mean and variance over the batch
+    mean, var = tf.nn.moments(input, [0])
+
+    # Reshape mean and variance tensors
+    input_shape = tf.shape(input).as_list()
+
+    # Define affine transformation parameters
+    beta = tf.Variable(tf.truncated_normal(input_shape[1:4]), name="beta")
+    gamma = tf.Variable(tf.truncated_normal(input_shape[1:4]), name="gamma")
+
+    # Combine gamma and variance into a single scaling term for efficiency
+    scale = tf.div(gamma, tf.sqrt(var + eps))
+
+    # Replicate tensors along batch dimension
+    mean_t = tf.tile(tf.expand_dims(mean, 0), [input_shape[0], 1, 1, 1])
+    beta_t = tf.tile(tf.expand_dims(beta, 0), [input_shape[0], 1, 1, 1])
+    scale_t = tf.tile(tf.expand_dims(scale, 0), [input_shape[0], 1, 1, 1])
+
+    # Apply normalization and affine transform
+    output = (input - mean_t) * scale_t + beta_t
 
     return output
